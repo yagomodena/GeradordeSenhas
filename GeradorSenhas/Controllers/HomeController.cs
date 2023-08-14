9f0485ddf4fc93cdf6fc17a79587366cc8fb867e
Yago <Yago@DESKTOP-59265J7>
2023-08-14 19:29:06 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorSenhas/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace PasswordGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GeradorSenhas(int tamanho)
+        {
+            string caracteresValidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
+            StringBuilder senha = new StringBuilder();
+
+            Random random = new Random();
+            for (int i = 0; i < tamanho; i++)
+            {
+                int index = random.Next(caracteresValidos.Length);
+                senha.Append(caracteresValidos[index]);
+            }
+
+            ViewData["GeradorSenhas"] = senha.ToString();
+            return View("Index");
+        }
+    }
+}
